d94c6a3778bc3abca3a0ebee1acaebc67b2f9001
siddharth1728 <siddharthreddy1728@gmail.com>
2025-06-12 19:08:52 +0530

    assignment

--- main.py
+++ main.py
@@ -1,5 +1,47 @@
-a = 45
 
-print(a + 45)
+#question 1
+list = [['jack','apple'],['orange', 'kiwi'],['dragon', 'grape']]
 
-b = 43
+print(list[2][0])
+
+#question2
+a = ((0,1),(2,3),(4,5))
+
+print(a[2][1])
+
+#question 3
+
+list_students = [('Alist',23),('Bob',21)]
+
+print(list_students[1][1])
+
+#question 4
+
+list1 = ([10,20],[30,40])
+
+print(list1[1][1])
+
+#question 5 
+
+students = {
+    "name1" : "siddharth",
+    "name2" : "abhi"
+    ""
+    }
+print(students["name1"])
+
+#question 6
+
+subject_marks = {
+    "maths" : 90,
+    "english" : 80,
+    "science" : 85
+}
+print(int(subject_marks["maths"][1]))
+#question 7
+billing_history = {
+    "customer1" : [100, 200, 300],
+    "customer2" : [150, 250, 350]
+}
+print(billing_history["customer1"])
+
