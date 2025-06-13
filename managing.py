632a3420c9cb26198b33a32e287ed2497c0c8948
siddharth1728 <siddharthreddy1728@gmail.com>
2025-06-13 23:18:27 +0530

    assignment

---
+++ managing.py
@@ -0,0 +1,17 @@
+managing_tasks = {
+
+"task1": input("enter task1: "),
+
+"task2": input("enter task2: "),
+
+"task3": input("enter task3: "),
+
+}
+
+managing_tasks ["task2"] = input("update task2: ")
+
+del managing_tasks ["task3"]
+
+
+
+print(managing_tasks)
