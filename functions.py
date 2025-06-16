f91819c0a0d2e68e66ecb94394dacdad8cdbc2d7
siddharth1728 <siddharthreddy1728@gmail.com>
2025-06-16 21:45:54 +0530

    assignment

--- functions.py
+++ functions.py
@@ -1,101 +1,95 @@
-# # #question1
-
-# # def greet_user(name):
-# #     print(f"Hello, {name}!")
-
-
-
-# # greet_user("Alice")
+# #question1
 
 
 
 
-# # #question 2
 
-# def check_temperature(temp):
+# #question 2
 
-#     if temp > 99:
-#         return "Fever"
-#     elif temp < 99:
-#         return "Normal"
-#     else:
-#         return "None"
+def check_temperature(temp):
+
+    if temp > 99:
+        return "Fever"
+    elif temp < 99:
+        return "Normal"
+    else:
+        return "None"
 
     
-# a = check_temperature(143)
-# print(a)
+a = check_temperature(143)
+print(a)
 
-# # #question 3
+# #question 3
 
-# fruits = ["apple", "banana", "cherry"]
-# def get_list_fruits(fruits):
-#     return fruits[-1]
+fruits = ["apple", "banana", "cherry"]
+def get_list_fruits(fruits):
+    return fruits[-1]
 
-# a = get_list_fruits(fruits)
+a = get_list_fruits(fruits)
 
-# print(a)
+print(a)
 
-# #question4
+#question4
 
-# def get_price_tag(price):
-#     if price > 1000:
-#         return "Expensive"
-#     else:
-#         return "Affordable" 
-# a = get_price_tag(1000)
+def get_price_tag(price):
+    if price > 1000:
+        return "Expensive"
+    else:
+        return "Affordable" 
+a = get_price_tag(1000)
 
-# print(a)
+print(a)
 
-# #question 5
-# def format_user_info(name,age):
-#     return f"Name: {name}, Age: {age}"
+#question 5
+def format_user_info(name,age):
+    return f"Name: {name}, Age: {age}"
 
-# a = format_user_info("Alice", 30)
-# print(a)   
+a = format_user_info("Alice", 30)
+print(a)   
 
-# #question 6
+#question 6
 
-# def uppercase_if_string(value):
-#     if value == "string":
-#         return value.upper()
-#     else :
-#         return "invalid input"
+def uppercase_if_string(value):
+    if value == "string":
+        return value.upper()
+    else :
+        return "invalid input"
     
-# a = uppercase_if_string("string")
-# print(a)
+a = uppercase_if_string("string")
+print(a)
 
-# #question 7
-# def safe_divide(num,den):
-#     result = num/den
-#     if den != 0:
-#         return result
+#question 7
+def safe_divide(num,den):
+    result = num/den
+    if den != 0:
+        return result
+    else:
+        return "cannot divide"
+
+a = safe_divide(5,2.5)
+print(float(a))
+
+#question 8
+
+# def check_login():
+#     username = input("Enter username: ")
+#     password = input("Enter password: ")
+#     if username == "admin" and password == "2468":
+#         return "Login successful"
 #     else:
-#         return "cannot divide"
+#         return "Login failed"
+# a = check_login()
+print(a)
 
-# a = safe_divide(5,2.5)
-# print(float(a))
+#question 9
 
-# #question 8
+def get_full_name(first_name, last_name):
+    return f"{first_name} {last_name}"
 
-# # def check_login():
-# #     username = input("Enter username: ")
-# #     password = input("Enter password: ")
-# #     if username == "admin" and password == "2468":
-# #         return "Login successful"
-# #     else:
-# #         return "Login failed"
-# # a = check_login()
-# # print(a)
+a = get_full_name("john", "doe")
+print(a.title())
 
-# #question 9
-
-# def get_full_name(first_name, last_name):
-#     return f"{first_name} {last_name}"
-
-# a = get_full_name("john", "doe")
-# print(a.title())
-
-# #question 10
+#question 10
 
 def get_discounted_price(price, is_memeber):
     if is_memeber:
