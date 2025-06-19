ff4e1b52fe6de4d7514350e5c6e60044e8e2ed94
siddharth1728 <siddharthreddy1728@gmail.com>
2025-06-19 21:06:11 +0530

    loops_assignment

--- loops.py
+++ loops.py
@@ -1,4 +1,4 @@
-
+#question1
 for numbers in range(1,6):
     print(numbers)
 
@@ -8,9 +8,9 @@ for numbers in range (10,20,2):
     print(numbers)
 
 #question3
-# names = [input("Enter a name: ") for _ in range(2)]
-# for name in names:
-#     print(name)
+names = [input("Enter a name: ") for _ in range(2)]
+for name in names:
+    print(name)
 
 #question4
 
@@ -29,7 +29,41 @@ details = [{
 }, {
     "name": "Charlie",
     "age": 35
-}]
+}] 
 
 for detail in details:
     print(f"{detail["name"]}, {detail["age"]}")
+
+# #question 6
+
+list_fruits = [["apple", "banana", ],["grape","mango"]]
+for fruit in list_fruits:
+    for item in fruit:
+        print(item)
+
+# question 7
+a = input("Enter a string: ")
+for char_ in a:
+    print(char_)
+    
+# question 8
+
+for i in range(5,0,-1):
+    print(i)
+
+# question 9
+
+details = [{
+    "subject": input("Enter subject: "),
+    "marks": int(input("Enter marks: "))
+   }
+    for _ in range(3)]
+for detail in details:
+    print(f"{detail['subject']}: {detail['marks']}")
+
+# question 10
+
+products = [("apple", 10), ("banana", 20), ("orange", 30)]
+for product in products:
+    name, price = product
+    print(f"Product: {name}, Price: {price}")
