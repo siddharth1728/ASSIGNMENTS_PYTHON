074a5a3e18001a83ae863f1af6c41aded66a0855
siddharth1728 <siddharthreddy1728@gmail.com>
2025-06-21 19:49:02 +0530

    practice

--- practice.py
+++ practice.py
@@ -1,52 +1,53 @@
-# # CONDITIONS
-# x = int(input("enter a number:"))
+# # # CONDITIONS
+# # x = int(input("enter a number:"))
+# # if x > 0:
+# #     print ("entered no_is positive")
+# # else:
+# #     print("entered no_is negative:")
+
+# # x = 2 
+
+# # if x < 7:
+# #     print("Hello")
+# # else:
+# #     print("Good morning")
+
+# # x = 2 
+
+# # if x > 7:
+# #     print("siddharth")
+# # elif x < 3:
+# #     print("chethan")
+# # else:
+# #     print("good morning")
+
+# # score = int(input("enter score:"))
+
+# # if score > 90:
+# #     print("Grade A")
+# # elif score >= 80:
+# #     print("Grade B")
+# # else:
+# #     print("Grade C")
+
+# # weather = input("enter weather condition:")
+# # if weather == "rainy":
+# #     print("carry an umbrella")
+# # elif weather == "sunny":
+# #     print("Wear sun glasses")
+# # else:
+# #     print("dont carry an umbrella")
+
+# # age = int(input("Enter Your Age:"))
+
+# # if age >= 18:
+# #     print("You are major")
+# # else:
+# #     print("You are minor")
+
+# x = 5
+# y = 10
+
 # if x > 0:
-#     print ("entered no_is positive")
-# else:
-#     print("entered no_is negative:")
+#     print("x is positive")
 
-# x = 2 
-
-# if x < 7:
-#     print("Hello")
-# else:
-#     print("Good morning")
-
-# x = 2 
-
-# if x > 7:
-#     print("siddharth")
-# elif x < 3:
-#     print("chethan")
-# else:
-#     print("good morning")
-
-# score = int(input("enter score:"))
-
-# if score > 90:
-#     print("Grade A")
-# elif score >= 80:
-#     print("Grade B")
-# else:
-#     print("Grade C")
-
-# weather = input("enter weather condition:")
-# if weather == "rainy":
-#     print("carry an umbrella")
-# elif weather == "sunny":
-#     print("Wear sun glasses")
-# else:
-#     print("dont carry an umbrella")
-
-# age = int(input("Enter Your Age:"))
-
-# if age >= 18:
-#     print("You are major")
-# else:
-#     print("You are minor")
-
-x = 5
-y = 10
-
-if x > 0:
-    print("")
