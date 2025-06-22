4d4f529ed2ce1ac7903e5585a43abc56e91d174d
siddharth1728 <siddharthreddy1728@gmail.com>
2025-06-22 15:11:25 +0530

    practice__

--- practice.py
+++ practice.py
@@ -1,53 +1,99 @@
-# # # CONDITIONS
-# # x = int(input("enter a number:"))
+# # # # CONDITIONS
+# # # x = int(input("enter a number:"))
+# # # if x > 0:
+# # #     print ("entered no_is positive")
+# # # else:
+# # #     print("entered no_is negative:")
+
+# # # x = 2 
+
+# # # if x < 7:
+# # #     print("Hello")
+# # # else:
+# # #     print("Good morning")
+
+# # # x = 2 
+
+# # # if x > 7:
+# # #     print("siddharth")
+# # # elif x < 3:
+# # #     print("chethan")
+# # # else:
+# # #     print("good morning")
+
+# # # score = int(input("enter score:"))
+
+# # # if score > 90:
+# # #     print("Grade A")
+# # # elif score >= 80:
+# # #     print("Grade B")
+# # # else:
+# # #     print("Grade C")
+
+# # # weather = input("enter weather condition:")
+# # # if weather == "rainy":
+# # #     print("carry an umbrella")
+# # # elif weather == "sunny":
+# # #     print("Wear sun glasses")
+# # # else:
+# # #     print("dont carry an umbrella")
+
+# # # age = int(input("Enter Your Age:"))
+
+# # # if age >= 18:
+# # #     print("You are major")
+# # # else:
+# # #     print("You are minor")
+
+# # x = 5
+# # y = 10
+
 # # if x > 0:
-# #     print ("entered no_is positive")
-# # else:
-# #     print("entered no_is negative:")
+# #     print("x is positive")
 
-# # x = 2 
+# sentence = "Hello, World!"
 
-# # if x < 7:
-# #     print("Hello")
-# # else:
-# #     print("Good morning")
+# age = 112
 
-# # x = 2 
+# print(sentence)
+# print(age)
 
-# # if x > 7:
-# #     print("siddharth")
-# # elif x < 3:
-# #     print("chethan")
-# # else:
-# #     print("good morning")
+# a = 2 + 3j
+# print(type(a))
+# print(a.real)
+# print(a.imag)
 
-# # score = int(input("enter score:"))
+# num = 3.14
+# print(type(num))
+# print(num)
 
-# # if score > 90:
-# #     print("Grade A")
-# # elif score >= 80:
-# #     print("Grade B")
-# # else:
-# #     print("Grade C")
+# sentence = "Hello, World!"
+# age = "112"
+# temp = 87.8
+# print(type(sentence))
+# print(type(age))
+# print(type(temp))
 
-# # weather = input("enter weather condition:")
-# # if weather == "rainy":
-# #     print("carry an umbrella")
-# # elif weather == "sunny":
-# #     print("Wear sun glasses")
-# # else:
-# #     print("dont carry an umbrella")
+# sentence = "Hello, World!"
 
-# # age = int(input("Enter Your Age:"))
+# can only contain letters and numbers 
 
-# # if age >= 18:
-# #     print("You are major")
-# # else:
-# #     print("You are minor")
+# ciwusydbuds_6473643_ = "Hello, World!"
+# print(ciwusydbuds_6473643_)
 
-# x = 5
-# y = 10
+# variable names are case sensitive 
 
-# if x > 0:
-#     print("x is positive")
+# key words cannot be used as variable names
+# for, while, if, else, elif, import, from, as, def,
 
+# anif = "Hello, World!"
+# print(anif)
+
+
+# phone_number_of_person = "123-456-7890"
+# print(phone_number_of_person)
+
+# string
+word = "beautiful"
+print(word[0])  # b# can access only through square brackets
+print(word[8])  # e
