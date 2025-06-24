a7ad6b493270650dd7b925374a78e2bef1d3cc5c
siddharth1728 <siddharthreddy1728@gmail.com>
2025-06-24 19:34:16 +0530

    oops

---
+++ oops.py
@@ -0,0 +1,55 @@
+class device:
+
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+    
+    def turn_on(self):
+        print(f"turning on {self.brand} ")
+
+    def turn_of(self):
+        print(f"turning off {self.brand} ")
+
+bike = device('duke','orange')
+
+print(bike.brand)
+print(bike.color)
+
+
+class device:
+
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+    
+    def turn_on(self):
+        print(f"turning on {self.brand} ")
+
+    def turn_of(self):
+        print(f"turning off {self.brand} ")
+
+car = device('audi', 'brown')
+
+print(car.brand)
+print(car.color)
+
+class device:
+
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+    
+    def turn_on(self):
+        print(f"turning on {self.brand} ")
+
+    def turn_of(self):
+        print(f"turning off {self.brand} ")
+
+pc = device('lenovo', 'grey')
+
+print(pc.brand)
+print(pc.color)
+
+
+
+    
--- practice.py
+++ practice.py
@@ -1,214 +1,325 @@
-# # # # # # CONDITIONS
-# # # # # x = int(input("enter a number:"))
-# # # # # if x > 0:
-# # # # #     print ("entered no_is positive")
-# # # # # else:
-# # # # #     print("entered no_is negative:")
+# # # # # # # # CONDITIONS
+# # # # # # # x = int(input("enter a number:"))
+# # # # # # # if x > 0:
+# # # # # # #     print ("entered no_is positive")
+# # # # # # # else:
+# # # # # # #     print("entered no_is negative:")
 
-# # # # # x = 2 
+# # # # # # # x = 2 
 
-# # # # # if x < 7:
-# # # # #     print("Hello")
-# # # # # else:
-# # # # #     print("Good morning")
+# # # # # # # if x < 7:
+# # # # # # #     print("Hello")
+# # # # # # # else:
+# # # # # # #     print("Good morning")
 
-# # # # # x = 2 
+# # # # # # # x = 2 
 
-# # # # # if x > 7:
-# # # # #     print("siddharth")
-# # # # # elif x < 3:
-# # # # #     print("chethan")
-# # # # # else:
-# # # # #     print("good morning")
+# # # # # # # if x > 7:
+# # # # # # #     print("siddharth")
+# # # # # # # elif x < 3:
+# # # # # # #     print("chethan")
+# # # # # # # else:
+# # # # # # #     print("good morning")
 
-# # # # # score = int(input("enter score:"))
+# # # # # # # score = int(input("enter score:"))
 
-# # # # # if score > 90:
-# # # # #     print("Grade A")
-# # # # # elif score >= 80:
-# # # # #     print("Grade B")
-# # # # # else:
-# # # # #     print("Grade C")
+# # # # # # # if score > 90:
+# # # # # # #     print("Grade A")
+# # # # # # # elif score >= 80:
+# # # # # # #     print("Grade B")
+# # # # # # # else:
+# # # # # # #     print("Grade C")
 
-# # # # # weather = input("enter weather condition:")
-# # # # # if weather == "rainy":
-# # # # #     print("carry an umbrella")
-# # # # # elif weather == "sunny":
-# # # # #     print("Wear sun glasses")
-# # # # # else:
-# # # # #     print("dont carry an umbrella")
+# # # # # # # weather = input("enter weather condition:")
+# # # # # # # if weather == "rainy":
+# # # # # # #     print("carry an umbrella")
+# # # # # # # elif weather == "sunny":
+# # # # # # #     print("Wear sun glasses")
+# # # # # # # else:
+# # # # # # #     print("dont carry an umbrella")
 
-# # # # # age = int(input("Enter Your Age:"))
+# # # # # # # age = int(input("Enter Your Age:"))
 
-# # # # # if age >= 18:
-# # # # #     print("You are major")
-# # # # # else:
-# # # # #     print("You are minor")
+# # # # # # # if age >= 18:
+# # # # # # #     print("You are major")
+# # # # # # # else:
+# # # # # # #     print("You are minor")
 
-# # # # x = 5
-# # # # y = 10
+# # # # # # x = 5
+# # # # # # y = 10
 
-# # # # if x > 0:
-# # # #     print("x is positive")
+# # # # # # if x > 0:
+# # # # # #     print("x is positive")
 
-# # # sentence = "Hello, World!"
+# # # # # sentence = "Hello, World!"
 
-# # # age = 112
+# # # # # age = 112
 
-# # # print(sentence)
-# # # print(age)
+# # # # # print(sentence)
+# # # # # print(age)
 
-# # # a = 2 + 3j
-# # # print(type(a))
-# # # print(a.real)
-# # # print(a.imag)
+# # # # # a = 2 + 3j
+# # # # # print(type(a))
+# # # # # print(a.real)
+# # # # # print(a.imag)
 
-# # # num = 3.14
-# # # print(type(num))
-# # # print(num)
+# # # # # num = 3.14
+# # # # # print(type(num))
+# # # # # print(num)
 
-# # # sentence = "Hello, World!"
-# # # age = "112"
-# # # temp = 87.8
-# # # print(type(sentence))
-# # # print(type(age))
-# # # print(type(temp))
+# # # # # sentence = "Hello, World!"
+# # # # # age = "112"
+# # # # # temp = 87.8
+# # # # # print(type(sentence))
+# # # # # print(type(age))
+# # # # # print(type(temp))
 
-# # # sentence = "Hello, World!"
+# # # # # sentence = "Hello, World!"
 
-# # # can only contain letters and numbers 
+# # # # # can only contain letters and numbers 
 
-# # # ciwusydbuds_6473643_ = "Hello, World!"
-# # # print(ciwusydbuds_6473643_)
+# # # # # ciwusydbuds_6473643_ = "Hello, World!"
+# # # # # print(ciwusydbuds_6473643_)
 
-# # # variable names are case sensitive 
+# # # # # variable names are case sensitive 
 
-# # # key words cannot be used as variable names
-# # # for, while, if, else, elif, import, from, as, def,
+# # # # # key words cannot be used as variable names
+# # # # # for, while, if, else, elif, import, from, as, def,
 
-# # # anif = "Hello, World!"
-# # # print(anif)
+# # # # # anif = "Hello, World!"
+# # # # # print(anif)
 
 
-# # # phone_number_of_person = "123-456-7890"
-# # # print(phone_number_of_person)
+# # # # # phone_number_of_person = "123-456-7890"
+# # # # # print(phone_number_of_person)
 
-# # # string
-# # # word = "beautiful"
-# # # print(word[0])  # b# can access only through square brackets
-# # # print(word[8])  # accesing elements with positive index
-# # # print(word[-1])  # l # accessing elements with negative index  
+# # # # # string
+# # # # # word = "beautiful"
+# # # # # print(word[0])  # b# can access only through square brackets
+# # # # # print(word[8])  # accesing elements with positive index
+# # # # # print(word[-1])  # l # accessing elements with negative index  
 
-# # # SLICING
-# # # word = "beautiful"
-# # # print(word[:5:]) 
+# # # # # SLICING
+# # # # # word = "beautiful"
+# # # # # print(word[:5:]) 
 
-# # # start = 0
-# # # stop = 8
-# # # print(word[start:stop:])
+# # # # # start = 0
+# # # # # stop = 8
+# # # # # print(word[start:stop:])
 
-# # # print(word[0:10]) # stop index fix with last index
+# # # # # print(word[0:10]) # stop index fix with last index
 
-# # # check string content
+# # # # # check string content
 
-# # # text = "heLlo$123"
+# # # # # text = "heLlo$123"
 
-# # # text = text.upper() #HELLO$123
+# # # # # text = text.upper() #HELLO$123
 
-# # # print(text.isalnum()) 
+# # # # # print(text.isalnum()) 
 
-# # # print(text.isdigit())
+# # # # # print(text.isdigit())
 
-# # # print(text.isupper())
+# # # # # print(text.isupper())
 
-# # # print(text.islower())
+# # # # # print(text.islower())
 
-# # # print(text.isspace())
+# # # # # print(text.isspace())
 
-# # #string formatting
-# # # name = "John"
-# # # amount = 5000
-# # # sentence = f"Dear {name}, your invoice of RS.{amount} is pending"
-# # # print(sentence)
+# # # # #string formatting
+# # # # # name = "John"
+# # # # # amount = 5000
+# # # # # sentence = f"Dear {name}, your invoice of RS.{amount} is pending"
+# # # # # print(sentence)
 
-# # #Dear "john", how are you?
-# # # name = "Dear 'john', how are you?"
-# # # print(name)
+# # # # #Dear "john", how are you?
+# # # # # name = "Dear 'john', how are you?"
+# # # # # print(name)
 
 
-# # #using f-strings(python 3.6+)
+# # # # #using f-strings(python 3.6+)
 
-# # # name = "John"
-# # # amount = 5000
+# # # # # name = "John"
+# # # # # amount = 5000
 
-# # # sentence = "Dear {}, your invoice is of Rs.{} pending".format(name,amount)
+# # # # # sentence = "Dear {}, your invoice is of Rs.{} pending".format(name,amount)
 
 
-# # # sentence = "Dear {1}, your invoice is of Rs.{0} pending".format(name,amount)
+# # # # # sentence = "Dear {1}, your invoice is of Rs.{0} pending".format(name,amount)
 
-# # # print(sentence)
+# # # # # print(sentence)
 
-# # name = "John"
-# # amount = 5000
+# # # # name = "John"
+# # # # amount = 5000
 
-# # # sentence = "Dear {}, your invoice is of Rs.{3} pending".format(name,amount)
+# # # # # sentence = "Dear {}, your invoice is of Rs.{3} pending".format(name,amount)
 
-# # sentence = "Dear {name}, your invoice is of Rs.{amount} pending".format(name="charlie",amount=2000)
+# # # # sentence = "Dear {name}, your invoice is of Rs.{amount} pending".format(name="charlie",amount=2000)
 
-# # a,b,*c = 2,4,6,8
-# # a, *b, c = 2,3,4,
-# # "*" can access elements except the assigned one
-# # print(a,b,c) #can only have exact no of values to axis from respective variable
+# # # # a,b,*c = 2,4,6,8
+# # # # a, *b, c = 2,3,4,
+# # # # "*" can access elements except the assigned one
+# # # # print(a,b,c) #can only have exact no of values to axis from respective variable
 
-# # multiple starred expressions not allowed
-# # mulltiline string used by triple quotes
+# # # # multiple starred expressions not allowed
+# # # # mulltiline string used by triple quotes
 
-# # sentence = "This is a rainy day"
+# # # # sentence = "This is a rainy day"
 
-# # email = """Dear Sanjay,
-# # How are you,
-# # Thanks"""
+# # # # email = """Dear Sanjay,
+# # # # How are you,
+# # # # Thanks"""
 
-# # print(email)
+# # # # print(email)
 
-# # Escape CHARACTER
+# # # # Escape CHARACTER
 
-# # sentence = 'It\'s a rainy day' #back slash is ignored by python
-# # print(sentence)
+# # # # sentence = 'It\'s a rainy day' #back slash is ignored by python
+# # # # print(sentence)
 
-# # print("hello", "world", sep=";")
+# # # # print("hello", "world", sep=";")
 
-# # a = input("Enter your name:")
-# # print(f"Hello {a}")
+# # # # a = input("Enter your name:")
+# # # # print(f"Hello {a}")
 
 
-# # a = "3"
-# # b = 4
+# # # # a = "3"
+# # # # b = 4
 
-# # print(a + b) #TypeError: unsupported operand type(s) for +: 'int' and 'str'
+# # # # print(a + b) #TypeError: unsupported operand type(s) for +: 'int' and 'str'
 
-# # a = 3/
-# # b = "4"
-# # c = int(b)
-# # print(float(a))
-# # print(float(b)) # should have only numeric values
+# # # # a = 3/
+# # # # b = "4"
+# # # # c = int(b)
+# # # # print(float(a))
+# # # # print(float(b)) # should have only numeric values
 
-# # print(str(a) + str(b))
+# # # # print(str(a) + str(b))
 
 
-# # a = int(input("Enter first number:"))
-# # b = int(input("Enter your second number:"))
+# # # # a = int(input("Enter first number:"))
+# # # # b = int(input("Enter your second number:"))
 
-# # print(f"The addition of {a} & {b} is : {a + b}")
-# # print(f"The multiplication of {a} & {b} is : {a * b}")
-# # print(f"The subtraction of {a} & {b} is : {a - b}")
-# # print(f"The division of {a} & {b} is : {a / b}")
+# # # # print(f"The addition of {a} & {b} is : {a + b}")
+# # # # print(f"The multiplication of {a} & {b} is : {a * b}")
+# # # # print(f"The subtraction of {a} & {b} is : {a - b}")
+# # # # print(f"The division of {a} & {b} is : {a / b}")
 
-# # ask user to enter credit card no
-# # print the credit number with last four digit masked with XXXX
-# output = "XXXX XXXX XXXX "
-# number = input("Enter credit card number:")
+# # # # ask user to enter credit card no
+# # # # print the credit number with last four digit masked with XXXX
+# # # output = "XXXX XXXX XXXX "
+# # # number = input("Enter credit card number:")
 
-# a = number[::-1]
-# print(a)
+# # # a = number[::-1]
+# # # print(a)
+
+
+# # a,b = 10,2
+
+# # # print(a//b) #float division
+
+# # # print(a % b) # modulus (coef div)
+
+# # # print(a ** b)/"*" - represents power
+
+# # # print(-a) # unary minus 
+
+# # # print(a) 3 unary plus
+
+# # a = a + b * a - (a ** b)
+
+# # print(a)
+
+# # # paranthesis
+# # # power
+# # # division
+# # # mul
+# # # add
+# # # sub
+
+# # 
+# # numbers = [2,3,4]
+
+# # a = 3 in numbers
+# # print(a)
+
+# # name = "Saurav"
+
+# # a = "u" in name
+# # a = "u" not in name
+# # print(a)
+
+
+# # assignment operators
+
+# # x = 10
+# # x += 1
+# # x-=1
+# # x *= x
+# # x /= 2
+# # x //= 2
+# # x = x**2
+# # x %= 5
+# # print(x)
+
+
+# # identity operators
+
+# # empty_list = []
+numbers = [1,2,3,4]
+# # mixed = [1 , "hello", 3.14, True]
+# # nested = [1, [2,3], [4,5]]
+
+# # print(empty_list, numbers, mixed, nested)
+
+# first = numbers[0]
+# last = numbers[-1]
+
+# print(first)
+# print(last)
+
+
+# a = numbers[1:4]
+# b = numbers[::1]
+
+# print(b)
+
+coordinates = (3,2,4,2,4,3)
+
+position = coordinates.index(4)
+position = coordinates.count(1)
+
+print(position)
+
+
+tuple1 = (1,2,3)
+
+
+tuple2 = (4,5,6)
+
+a = tuple1 + tuple2
+
+print(a)
+
+print(tuple1 * 2)
+
+
+length = len(tuple1)
+
+print(length)
+
+exists = 2 in tuple1
+not_exists = 5 in tuple1
+
+print(exists,not_exists)
+
+minimum = min(tuple1)
+maximum = max(tuple2)
+
+print(minimum, maximum )
+
+print(sum(tuple1))
+
+
+
+
 
