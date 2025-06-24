9ed679545de2a6ba7da4177da0d9927c67891863
siddharth1728 <siddharthreddy1728@gmail.com>
2025-06-24 22:46:00 +0530

    oops

--- oops.py
+++ oops.py
@@ -1,54 +1,150 @@
-class device:
+# class device:
 
-    def __init__(self, brand, color):
-        self.brand = brand
-        self.color = color
+#     def __init__(self, brand, color):
+#         self.brand = brand
+#         self.color = color
     
-    def turn_on(self):
-        print(f"turning on {self.brand} ")
+#     def turn_on(self):
+#         print(f"turning on {self.brand} ")
 
-    def turn_of(self):
-        print(f"turning off {self.brand} ")
+#     def turn_of(self):
+#         print(f"turning off {self.brand} ")
 
-bike = device('duke','orange')
+# bike = device('duke','orange')
 
-print(bike.brand)
-print(bike.color)
+# print(bike.brand)
+# print(bike.color)
 
 
-class device:
+# class device:
 
-    def __init__(self, brand, color):
-        self.brand = brand
-        self.color = color
+#     def __init__(self, brand, color):
+#         self.brand = brand
+#         self.color = color
     
-    def turn_on(self):
-        print(f"turning on {self.brand} ")
+#     def turn_on(self):
+#         print(f"turning on {self.brand} ")
 
-    def turn_of(self):
-        print(f"turning off {self.brand} ")
+#     def turn_of(self):
+#         print(f"turning off {self.brand} ")
 
-car = device('audi', 'brown')
+# car = device('audi', 'brown')
 
-print(car.brand)
-print(car.color)
+# print(car.brand)
+# print(car.color)
 
-class device:
+# class device:
 
-    def __init__(self, brand, color):
-        self.brand = brand
-        self.color = color
+#     def __init__(self, brand, color):
+#         self.brand = brand
+#         self.color = color
     
-    def turn_on(self):
-        print(f"turning on {self.brand} ")
+#     def turn_on(self):
+#         print(f"turning on {self.brand} ")
 
-    def turn_of(self):
-        print(f"turning off {self.brand} ")
+#     def turn_of(self):
+#         print(f"turning off {self.brand} ")
 
-pc = device('lenovo', 'grey')
+# pc = device('lenovo', 'grey')
 
-print(pc.brand)
-print(pc.color)
+# print(pc.brand)
+# print(pc.color)
+
+
+# question 2
+
+class Car:
+
+    def __init__(self,model_no, speed, fuel_type):
+        self.model_no = model_no
+        self.speed = speed
+        self.fuel_type = fuel_type
+
+    def accelerate(self):
+        print(f" {self.model_no} is accelerating at {self.speed}km/(hr)^2 ...")
+
+    def stop(self):
+        print(f" {self.model_no} is stopped")
+
+    def honk(self):
+        print(f" {self.model_no} Honk !!")
+
+car = Car('audi_r8', '60', 'Petrol')
+
+car.accelerate()
+car.stop()
+car.honk()
+
+
+print("    ")
+
+
+
+class Bike:
+
+    def __init__(self,model_no, speed, fuel_type):
+        self.model_no = model_no
+        self.speed = speed
+        self.fuel_type = fuel_type
+
+    def accelerate(self):
+        print(f" {self.model_no} is accelerating at {self.speed}km/(hr)^2 ...")
+
+    def stop(self):
+        print(f" {self.model_no} is stopped")
+
+    def honk(self):
+        print(f" {self.model_no} Honk !!")
+
+    
+bike = Bike('Royal_Enfield_classic_650', '80', 'Petrol')
+
+bike.accelerate()
+bike.stop()
+bike.honk()
+
+
+
+print(" ")
+
+
+
+class Truck:
+
+    def __init__(self,model_no, speed, fuel_type):
+        self.model_no = model_no
+        self.speed = speed
+        self.fuel_type = fuel_type
+
+    def accelerate(self):
+        print(f" {self.model_no} is accelerating at {self.speed}km/(hr)^2 ...")
+
+    def stop(self):
+        print(f" {self.model_no} is stopped")
+
+    def honk(self):
+        print(f" {self.model_no} Honk !!")
+
+truck = Truck('Ashok_Leyland', '67', 'Diesel')
+
+truck.accelerate()
+truck.stop()
+truck.honk()
+
+#Question3
+
+class Teacher:
+    def __init__(self, Name, subject):
+        self.Teacher = Teacher
+        self.Student = Student
+
+    
+
+    
+
+
+
+
 
 
 
